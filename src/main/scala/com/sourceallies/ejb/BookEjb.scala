b4306e0629d8ba4126c89292f37bd6b3161fbab6
Zach Cox <zcox522@gmail.com>
2010-01-26 08:32:47 -0500

    Clean up

--- src/main/scala/com/sourceallies/ejb/BookEjb.scala
+++ src/main/scala/com/sourceallies/ejb/BookEjb.scala
@@ -8,17 +8,3 @@ import com.sourceallies.model._
 @Stateless
 @LocalBean
 class BookEjb extends CrudEjb[Book]
-
-/*@Stateless
-@LocalBean
-class BookEjb {
-  @PersistenceContext{val unitName = "book"}
-  private var manager: EntityManager = _
-
-  def findBooks: java.util.List[_] = manager createNamedQuery "findAllBooks" getResultList
-
-  def createBook(book: Book) = { 
-    manager persist book
-    book
-  }
-}*/
